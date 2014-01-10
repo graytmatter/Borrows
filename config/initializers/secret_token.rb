34fcb30801a66c246336b1a1b802028f125cbbb2
James Dong <jdong8@gmail.com>
2014-01-09 23:25:47 -0800

    Google form test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleTest::Application.config.secret_key_base = 'cfcae45741e61602685bad14000952deb56d570a74da7298cf8c24b43c37da3ed2d20258b245895ab0e9bea88e4b4b36d525bd370d439c73ecbe02fa467f25e9'
