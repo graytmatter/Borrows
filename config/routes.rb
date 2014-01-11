211dbbc47da0ab4b2cca0937c20ad15b447be52b
James Dong <jdong8@gmail.com>
2014-01-10 22:54:54 -0800

    root back to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 GoogleTest::Application.routes.draw do
-  #root 'static_pages#new'
-  root 'requests#new'
+  root 'static_pages#new'
+  #root 'requests#new'
 
   get 'edit/:edit_id', to: 'requests#edit', as: 'edit_request'
   post 'requests', to: 'requests#create' 
