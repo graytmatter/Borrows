8e6eda6af02e49099627f5673b94a6e75e58f0b2
James Dong <jdong8@gmail.com>
2014-01-10 22:37:25 -0800

    Model and view code

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,20 @@
 class Request < ActiveRecord::Base
+  before_create :create_edit_id
+
+
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :item, presence: true
+  validates :detail, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+
+
+  private
+
+    def Request.new_edit_id
+      SecureRandom.urlsafe_base64
+    end
+
+    def create_edit_id
+      self.edit_id = Request.new_edit_id
+    end
 end
