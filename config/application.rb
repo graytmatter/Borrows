8171a361333659d9b55b1ad05a0ce854918726b7
James Dong <jdong8@gmail.com>
2014-01-20 21:21:22 -0800

    precompile yet again changing config file

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module GoogleTest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(custom.css.scss home.css.scss *.png *.jpg *.jpeg *.gif)
   end
 end
