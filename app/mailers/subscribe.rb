a802f82ed97a31aece9f644d24bf9806f8d8992c
James Dong <jdong8@gmail.com>
2014-02-24 23:56:46 -0800

    automated mail subscriptions (new files)

---
+++ app/mailers/subscribe.rb
@@ -0,0 +1,6 @@
+class Subscribe < ActionMailer::Base
+  def notification_email(signup)
+    @signup = signup
+    mail(to: @signup.email, from: ENV['owner'], :subject => "Thanks for signing up @ borrows.herokuapp.com") 
+  end
+end
