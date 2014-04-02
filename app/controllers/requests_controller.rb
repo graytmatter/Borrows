1996bbe2ef3b86d200c16c0d653201029f043dde
James Dong <jdong8@gmail.com>
2014-04-02 12:18:58 -0700

    removed item redundancy

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -67,8 +67,8 @@ http_basic_authenticate_with :name => "borrower", :password => "bigmooch"
     def inventory
       @inventory = {
       "Camping" => ["Tent", "Sleeping bag", "Sleeping pad", "Backpack", "Water filter"],
-      "Sports & outdoors" => ["Tennis set", "Volleyball set", "Bicycle pump", "Portable grill", "Cooler"],
-      "Tools" => ["Electric drill", "Screwdriver set", "Hammer", "Wrench set", "Vacuum"],
+      "Sports & outdoors" => ["Tennis set", "Volleyball set", "Bicycle pump", "Outdoors grill", "Cooler"],
+      "Tools" => ["Electric drill", "Screwdriver set", "Hammer", "Wrench set", "Extension cords"],
       "Home & kitchen" => ["Vacuum", "Air mattress", "Blender", "Electric grill", "Food processor"],
       "Baby gear" => ["Umbrella Stroller", "Booster seat", "Backpack carrier", "Pack n' Play", "Jumper"],
       "Snow sports" => ["Outerwear", "Innerwear", "Gloves" , "Helmet", "Goggles"]
