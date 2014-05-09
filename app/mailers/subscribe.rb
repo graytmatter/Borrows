754c269f3b1558b7c68315fc8c9c090784dfd84a
James Dong <jdong8@gmail.com>
2014-05-08 22:02:09 -0700

    finished

--- app/mailers/subscribe.rb
+++
@@ -1,6 +0,0 @@
-class Subscribe < ActionMailer::Base
-  def notification_email(signup)
-    @signup = signup
-    mail(to: @signup.email, from: ENV['owner'], bcc: ENV['owner'], :subject => "Thanks for signing up @ borrows.herokuapp.com") 
-  end
-end
--- app/views/entry_codes/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @entry_code, :id, :code, :active, :created_at, :updated_at
