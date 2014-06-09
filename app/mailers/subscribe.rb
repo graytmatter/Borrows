dd0962bf4aa811eefd44ad8e1526342ef70a3f2b
James Dong <jdong8@gmail.com>
2014-06-09 14:28:12 -0700

    quick updates to signup mailer

--- app/mailers/subscribe.rb
+++ app/mailers/subscribe.rb
@@ -1,6 +1,6 @@
 class Subscribe < ActionMailer::Base
   def notification_email(signup)
     @signup = signup
-    mail(to: @signup.email, from: ENV['owner'], :subject => "Thanks for visiting our borrowing site!") 
+    mail(to: @signup.email, from: ENV['owner'], :subject => "Thanks for visiting Project Borrow!") 
   end
 end
