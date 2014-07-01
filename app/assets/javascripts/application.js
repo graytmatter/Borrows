eff266bb5cd9bb01f4ebfb0e30ac718c9599faeb
James Dong <jdong8@gmail.com>
2014-06-30 21:24:37 -0700

    tweaked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.touch-punch
+//= require jquery.ui.touch-punch.min.js
 //= require_tree .
 //= require bootstrap
 //= require photopile
