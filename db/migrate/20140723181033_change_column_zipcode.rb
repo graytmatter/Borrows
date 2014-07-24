e6e79db7ca6743ac901f300513a7a4a1ac78610a
James Dong <jdong8@gmail.com>
2014-07-23 22:10:26 -0700

    raw sql into final migraiton file

--- db/migrate/20140723181033_change_column_zipcode.rb
+++ db/migrate/20140723181033_change_column_zipcode.rb
@@ -1,5 +1,5 @@
 class ChangeColumnZipcode < ActiveRecord::Migration
   def change
-  	change_column :signups, :zipcode, :integer
+  	execute 'ALTER TABLE signups ALTER zipcode TYPE integer USING zipcode::int;'
   end
 end
