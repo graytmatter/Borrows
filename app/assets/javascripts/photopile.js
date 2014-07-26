d3b5c562343d780986af3aac6b7bf478ab056597
James Dong <jdong8@gmail.com>
2014-07-26 02:02:45 -0700

    tweaked photopile

--- app/assets/javascripts/photopile.js
+++ app/assets/javascripts/photopile.js
@@ -13,7 +13,7 @@ var photopile = (function() {
 
     // Thumbnails
     var numLayers         = 12;          // number of layers in the pile (max zindex)
-    var thumbOverlap      = 150;         // overlap amount (px)
+    var thumbOverlap      = 100;         // overlap amount (px)
     var thumbRotation     = 45;         // maximum rotation (deg)
     var thumbBorderWidth  = 2;          // border width (px)
     var thumbBorderColor  = 'white';    // border color
