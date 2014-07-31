aa828b4513855f5af615622fbaeda99bda830681
James Dong <jdong8@gmail.com>
2014-07-31 10:51:53 -0700

    testing to make one job happen

--- app/jobs/recurring.rb
+++ app/jobs/recurring.rb
@@ -2,25 +2,25 @@ class Recurring
 	include SuckerPunch::Job
 	include FistOfFury::Recurrent
 
-	recurs { daily }
+	recurs { minutely }
 
 	def perform
 		ActiveRecord::Base.connection_pool.with_connection do 
 
-			# Auto set status to in progress from connected
-			Borrow.where(status1: 2).select { |b| b.request.pickupdate == Date.today }.each { |b| b.update_attributes(status1 == 3 ) }
+			# # Auto set status to in progress from connected
+			# Borrow.where(status1: 2).select { |b| b.request.pickupdate == Date.today }.each { |b| b.update_attributes(status1 == 3 ) }
 
-			# Auto set status to complete and connect borrowers/lenders for return
-			Borrow.where(status1: 3).select { |b| b.request.returndate == Date.today }.each do |b| 
-				b.update_attributes(status1 == 4 ) 
-				RequestMailer.return_reminder(b).deliver
-			end
+			# # Auto set status to complete and connect borrowers/lenders for return
+			# Borrow.where(status1: 3).select { |b| b.request.returndate == Date.today }.each do |b| 
+			# 	b.update_attributes(status1 == 4 ) 
+			# 	RequestMailer.return_reminder(b).deliver
+			# end
 
-			# Auto set status to N/A if borrows are still outstanding on their pickup date
-			Borrow.where(status1: 1).select { |b| b.request.pickupdate == Date.today}.each do |b|
-				b.update_attributes(status1 == 20)
-				RequestMailer.not_found(b, b.itemlist_id).deliver
-			end
+			# # Auto set status to N/A if borrows are still outstanding on their pickup date
+			# Borrow.where(status1: 1).select { |b| b.request.pickupdate == Date.today}.each do |b|
+			# 	b.update_attributes(status1 == 20)
+			# 	RequestMailer.not_found(b, b.itemlist_id).deliver
+			# end
 
 			# Auto email lenders every other day if they have outstanding requests
 			lender_array = Array.new
