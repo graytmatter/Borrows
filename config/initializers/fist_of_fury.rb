77c2fb8a64752d398a1a4c2a9a579456f98a969e
James Dong <jdong8@gmail.com>
2014-08-01 16:25:39 -0700

    tweaks to ice cube language

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -1,9 +1,9 @@
 # Ensure the jobs run only in a web server.
 if defined?(Rails::Server)
   FistOfFury.attack! do
-    Inprogress.recurs { daily.hour_of_the_day(23) }
-    Return.recurs { daily.hour_of_the_day(5) }
-    Noresponse.recurs { daily.hour_of_the_day(23) }
-    Outstanding.recurs { daily.hour_of_the_day(1) }
+    Inprogress.recurs { daily.hour_of_day(23) }
+    Return.recurs { daily.hour_of_day(5) }
+    Noresponse.recurs { daily.hour_of_day(23) }
+    Outstanding.recurs { daily.hour_of_day(1) }
   end
 end
