c46a312c69fe94e2dd4236ca5ac4784715c2fc3b
James Dong <jdong8@gmail.com>
2014-08-05 17:11:13 -0700

    changed to utc time

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -1,5 +1,10 @@
 # Ensure the jobs run only in a web server.
 if defined?(Rails::Server)
+
+FistOfFury.configure do |config|
+  config.utc = true # false by default; makes all time within Fist of Fury UTC
+end
+
   FistOfFury.attack! do
     Inprogress.recurs { daily.hour_of_day(23) }
     Return.recurs { daily.hour_of_day(5) }
