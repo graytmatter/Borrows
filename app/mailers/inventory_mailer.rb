d407ad93060840654636a4e0b7f0c245d51adc4a
James Dong <jdong8@gmail.com>
2014-08-06 00:42:07 -0700

    added logger

--- app/mailers/inventory_mailer.rb
+++ app/mailers/inventory_mailer.rb
@@ -6,5 +6,8 @@ class InventoryMailer < ActionMailer::Base
     Inventory.where(signup_id: lender_id).select { |i| Borrow.where(inventory_id: i.id).select { |b| b.status1 == 1}.count > 0 }.each { |i| @items_outstanding << Itemlist.find_by_id(i.itemlist_id).name }
     # mail(to: @lender_email, from: ENV['owner'], :subject => "[Project Borrow]: Can someone borrow your stuff?")
     mail(to: "jdong8@gmail.com", from: ENV['owner'], :subject => "[Project Borrow]: Can someone borrow your stuff?")
-	end
+    
+    logger.debug "sent mail to #{@lender_email} at local time: #{Time.now.getlocal}, UTC time: #{Time.now.getutc}"
+
+  end
 end
