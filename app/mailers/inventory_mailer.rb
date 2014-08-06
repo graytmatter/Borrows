ea6b77fda93be87c3fa3e9010a5f0b3d9df700c6
James Dong <jdong8@gmail.com>
2014-08-06 10:21:27 -0700

    added new time line

--- app/mailers/inventory_mailer.rb
+++ app/mailers/inventory_mailer.rb
@@ -7,7 +7,7 @@ class InventoryMailer < ActionMailer::Base
     # mail(to: @lender_email, from: ENV['owner'], :subject => "[Project Borrow]: Can someone borrow your stuff?")
     mail(to: "jdong8@gmail.com", from: ENV['owner'], :subject => "[Project Borrow]: Can someone borrow your stuff?")
     
-    logger.debug "sent mail to #{@lender_email} at local time: #{Time.now.getlocal}, UTC time: #{Time.now.getutc}"
+    logger.debug "sent mail to #{@lender_email} at local time: #{Time.now.in_time_zone('Pacific Time (US & Canada)')}, UTC time: #{Time.now.getutc}"
 
   end
 end
