ebf736f46360cafd9f0163903d5a28f3d80134aa
James Dong <jdong8@gmail.com>
2014-08-06 18:00:52 -0700

    twaek

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -23,52 +23,56 @@ class StaticpagesController < ApplicationController
 		@total_requests = Request.count
 			@avg_borrow_per_request = Borrow.count / Request.count
 
-			@same_day_requests = Request.select { |r| r.created_at.to_date == r.pickupdate.to_date }.count
-			@three_day_advance_requests = Request.select { |r| 3 >= (r.pickupdate.to_date - r.created_at.to_date).to_i && r.created_at.to_date != r.pickupdate.to_date }.count
-			@one_week_advance_requests = Request.select { |r| 7 >= (r.pickupdate.to_date - r.created_at.to_date).to_i && (r.pickupdate.to_date - r.created_at.to_date).to_i > 3 }.count
-			@beyond_1_week_advance_requests = Request.select { |r| (r.pickupdate.to_date - r.created_at.to_date).to_i > 7 }.count
+				request_with_dates = Request.where("created_at is not null").where("pickupdate is not null").where("returndate is not null") 
+			@same_day_requests = request_with_dates.select { |r| r.created_at.to_date == r.pickupdate.to_date }.count
+			@three_day_advance_requests = request_with_dates.select { |r| 3 >= (r.pickupdate.to_date - r.created_at.to_date).to_i && r.created_at.to_date != r.pickupdate.to_date }.count
+			@one_week_advance_requests = request_with_dates.select { |r| 7 >= (r.pickupdate.to_date - r.created_at.to_date).to_i && (r.pickupdate.to_date - r.created_at.to_date).to_i > 3 }.count
+			@beyond_1_week_advance_requests = request_with_dates.select { |r| (r.pickupdate.to_date - r.created_at.to_date).to_i > 7 }.count
 
 				sum_of_request_notice = 0
-				Request.all.each do |r|
+				request_with_dates.all.each do |r|
 					sum_of_request_notice += (r.pickupdate.to_date - r.created_at.to_date).to_i
 				end
-			@avg_advance_notice = sum_of_request_notice / Request.count
+			@avg_advance_notice = sum_of_request_notice / request_with_dates.count
 
-			@day_long_requests = Request.select { |r| r.returndate.to_date == r.pickupdate.to_date }.count
-			@three_day_long_requests = Request.select { |r| 3 >= (r.returndate.to_date - r.pickupdate.to_date).to_i }.count
-			@one_week_long_requests = Request.select { |r| 7 >= (r.returndate.to_date - r.pickupdate.to_date).to_i && (r.returndate.to_date - r.pickupdate.to_date).to_i > 3}.count
-			@beyond_1_week_long_requests = Request.select { |r| (r.returndate.to_date - r.pickupdate.to_date).to_i > 7}.count
+			@day_long_requests = request_with_dates.select { |r| r.returndate.to_date == r.pickupdate.to_date }.count
+			@three_day_long_requests = request_with_dates.select { |r| 3 >= (r.returndate.to_date - r.pickupdate.to_date).to_i }.count
+			@one_week_long_requests = request_with_dates.select { |r| 7 >= (r.returndate.to_date - r.pickupdate.to_date).to_i && (r.returndate.to_date - r.pickupdate.to_date).to_i > 3}.count
+			@beyond_1_week_long_requests = request_with_dates.select { |r| (r.returndate.to_date - r.pickupdate.to_date).to_i > 7}.count
 
 				sum_of_request_time = 0
-				Request.all.each do |r|
+				request_with_dates.all.each do |r|
 					sum_of_request_time += (r.returndate.to_date - r.pickupdate.to_date).to_i
 				end
-			@avg_borrow_length = sum_of_request_time / Request.count
+			@avg_borrow_length = sum_of_request_time / request_with_dates.count
 
 
 		@total_borrows = Borrow.count
-			@borrows_did_use_PB = Borrow.where(status1: Status.where(statuscategory_id: 1).pluck("id")).count
 
-			@borrows_did_not_use_PB = Borrow.where(status1: Status.where(statuscategory_id: 2).pluck("id")).count
-				@borrows_true_cancel = Borrow.where(status1: [17..22, 36, 38]).count
-				@borrows_false_cancel = Borrow.where(status1: [8..16]).count
+				borrows_with_status = Borrow.where("status1 is not null")
+			@borrows_did_use_PB = borrows_with_status.where(status1: Status.where(statuscategory_id: 1).pluck("id")).count
+
+			@borrows_did_not_use_PB = borrows_with_status.where(status1: Status.where(statuscategory_id: 2).pluck("id")).count
+				@borrows_true_cancel = borrows_with_status.where(status1: [17..22, 36, 38]).count
+				@borrows_false_cancel = borrows_with_status.where(status1: [8..16]).count
 
 					@borrows_statuses = Hash.new
 					Status.where(statuscategory_id: [2]).each do |s|
-						@borrows_statuses[s.name] = Borrow.where(status1: s.id).count
+						@borrows_statuses[s.name] = borrows_with_status.where(status1: s.id).count
 					end
 				@borrows_statuses.reject! { |k, v| v == 0 }
 
-				itemlist_array_b = Borrow.pluck("itemlist_id")
+				borrows_with_itemlist_id = Borrow.where("itemlist_id is not null")
+				itemlist_array_b = borrows_with_itemlist_id.pluck("itemlist_id")
 				itemlist_array_b.map! { |i| Itemlist.find_by_id(i).name }
 				@items_count_b = Hash.new(0)
-				itemlist_array_b.each do |v|
-					@items_count_b[v] += 1
-				end
+			itemlist_array_b.each do |v|
+				@items_count_b[v] += 1
+			end
 
 
 		@total_inventories = Inventory.count
-
+		
 				itemlist_array_l = Inventory.pluck("itemlist_id")
 				itemlist_array_l.map! { |i| Itemlist.find_by_id(i).name }
 				@items_count_l = Hash.new(0)
