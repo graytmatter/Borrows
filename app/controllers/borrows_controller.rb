f4921758f6930f564eac988a98d02e566c74f559
James Dong <jdong8@gmail.com>
2014-08-07 20:36:22 -0700

    ransack lender email search working

--- app/controllers/borrows_controller.rb
+++ app/controllers/borrows_controller.rb
@@ -2,28 +2,24 @@ class BorrowsController < ApplicationController
   before_filter :authenticate
   
   def index
-    @q = Borrow.ransack(params[:q])
-    @borrows = @q.result.includes(:request => :signup)
-
-    @inventory_id_collection = Array.new
+    @inventory_id_collection = Hash.new
     Borrow.select { |b| b.inventory_id.present? }.each do |b|
-      if @inventory_id_collection.select{ |c| c.first == "#{Inventory.find_by_id(b.inventory_id).signup.email}" }.present?
-        @inventory_id_collection.select{ |c| c.first == "#{Inventory.find_by_id(b.inventory_id).signup.email}"}.first.second.push(b.inventory_id)
+      if @inventory_id_collection.has_key?("#{Inventory.find_by_id(b.inventory_id).signup.email}")
+        @inventory_id_collection["#{Inventory.find_by_id(b.inventory_id).signup.email}"] << b.inventory_id
       else
-        @inventory_id_collection << ["#{Inventory.find_by_id(b.inventory_id).signup.email}", [b.inventory_id]]
+        @inventory_id_collection["#{Inventory.find_by_id(b.inventory_id).signup.email}"] = Array.new
+        @inventory_id_collection["#{Inventory.find_by_id(b.inventory_id).signup.email}"] << b.inventory_id
       end
     end
 
-    # @inventory_id_collection = Hash.new
-    # Borrow.select { |b| b.inventory_id.present? }.each do |b|
-    #   if @inventory_id_collection.has_key?("#{Inventory.find_by_id(b.inventory_id).signup.email}")
-    #     @inventory_id_collection["#{Inventory.find_by_id(b.inventory_id).signup.email}"] << b.inventory_id
-    #   else
-    #     @inventory_id_collection["#{Inventory.find_by_id(b.inventory_id).signup.email}"] = Array.new
-    #     @inventory_id_collection["#{Inventory.find_by_id(b.inventory_id).signup.email}"] << b.inventory_id
-    #   end
-    # end
+    @inventory_id_collection.each do |k,v|
+      @inventory_id_collection[k] = v*","
+    end
 
+    query = params[:q]
+    query["inventory_id_eq_any"] = params[:q]["inventory_id_eq_any"].split(',') if query != nil
+    @q = Borrow.ransack(query)
+    @borrows = @q.result.includes(:request => :signup)
   end
 
   def edit
