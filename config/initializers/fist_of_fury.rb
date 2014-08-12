c4c3f73da1ee30bbaef85abc7f4d8ccd68e83a6c
James Dong <jdong8@gmail.com>
2014-08-11 19:36:16 -0700

    finished improved request view

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -1,6 +1,8 @@
 # Ensure the jobs run only in a web server.
 #if defined?(Rails::Server)
 
+if Rails.env != "development"
+
   FistOfFury.configure do |config|
     config.utc = true # false by default; makes all time within Fist of Fury UTC
   end
@@ -11,4 +13,5 @@
     # Noresponse.recurs { daily.hour_of_day(23) }
     Outstanding.recurs { daily.hour_of_day(18) }
   end
+end
 #end
