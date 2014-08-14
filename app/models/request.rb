a0e9b9ae504eb8fab15dd0dfd899da7567dd3d2a
James Dong <jdong8@gmail.com>
2014-08-14 13:12:12 -0700

    deleted commented out code in models

--- app/models/request.rb
+++ app/models/request.rb
@@ -18,67 +18,6 @@ class Request < ActiveRecord::Base
     end
   end
 
-# need a lot of manual adding, roughly 100 requests may only have 1 borrow when they should have several
-
-# FOURTH
-# Borrow.where("itemlist_id is null").each do |t| 
-#   if Itemlist.find_by_name(t.name)
-#     t.update_attributes(itemlist_id: Itemlist.find_by_name(t.name).id)
-#   end
-# end
-# then manually update the rest
-
-# Check 
-# fourthtest = []
-# Borrow.where("itemlist_id is not null").each do |t|
-#   if t.name == Itemlist.find_by_id(t.itemlist_id).name
-#     fourthtest << "OK"
-#   else
-#     fourthtest << "Error at #{t.id}"
-#   end
-# end
-
-# fourthtest
-
-
-# Inventory.where("itemlist_id is null").each do |i|
-#   if Itemlist.find_by_name(i.item_name).present?
-#     i.update_attributes(itemlist_id: Itemlist.find_by_name(i.item_name).id)
-#   end
-# end
-
-# FINAL CHECKS
-
-# Check that all borrows belong to a Request
-# Check that all borrows have an Itemlist
-# Check that all borrows have two statuses
-# Check that all Inventories have an Itemlist ID
-
-# Check that all borrows has a itemlist_id field and a request_id field
-
-# DELETE EXTRANEOUS COLUMNS YAY!""
-
-=begin
-  def update_spreadsheet
-    connection = GoogleDrive.login(ENV['g_username'], ENV['g_password'])
-    ss = connection.spreadsheet_by_title(ENV['spreadsheet_title'])
-    ws = ss.worksheets[0]
-    row = get_cell(self.edit_id)
-    ws[row, 2] = Time.new
-    ws[row, 3] = self.items
-    ws[row, 4] = self.detail
-    ws[row, 5] = self.email
-    ws[row, 6] = self.name
-    ws.save
-  end
-=end
-
-=begin #supports entry_codes
-  def to_param
-    self.edit_id
-  end
-=end
-
   private
 
     def Request.new_edit_id
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -21,34 +21,34 @@ class Signup < ActiveRecord::Base
         self.email = self.email.downcase
     end
 
-def save_subscrip
-    connection = GoogleDrive.login(ENV['GMAIL_USERNAME'], ENV['GMAIL_PASSWORD'])
-    if Rails.env == "production"
-        ss = connection.spreadsheet_by_title('Visitor spreadsheet') 
-    else
-        ss = connection.spreadsheet_by_title('Visitor spreadsheet old')
+    def save_subscrip
+        connection = GoogleDrive.login(ENV['GMAIL_USERNAME'], ENV['GMAIL_PASSWORD'])
+        if Rails.env == "production"
+            ss = connection.spreadsheet_by_title('Visitor spreadsheet') 
+        else
+            ss = connection.spreadsheet_by_title('Visitor spreadsheet old')
+        end
+        ws = ss.worksheets[0]
+        row = 1 + ws.num_rows 
+        ws[row, 1] = Time.new 
+        ws[row, 2] = self.name
+        ws[row, 3] = self.email
+        ws[row, 4] = self.heard
+        ws.save
     end
-    ws = ss.worksheets[0]
-    row = 1 + ws.num_rows 
-    ws[row, 1] = Time.new 
-    ws[row, 2] = self.name
-    ws[row, 3] = self.email
-    ws[row, 4] = self.heard
-    ws.save
-end
 
 =begin
-def add_subscrip
-    mailchimp = Gibbon::API.new 
-    result = mailchimp.lists.subscribe({ 
-        :id => ENV['MAILCHIMP_LIST_ID'], 
-        :email => {:email => self.email}, 
-        :double_optin => false, 
-        :update_existing => true, 
-        :send_welcome => true 
-    })
-    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
-end
+    def add_subscrip
+        mailchimp = Gibbon::API.new 
+        result = mailchimp.lists.subscribe({ 
+            :id => ENV['MAILCHIMP_LIST_ID'], 
+            :email => {:email => self.email}, 
+            :double_optin => false, 
+            :update_existing => true, 
+            :send_welcome => true 
+        })
+        Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+    end
 =end
 
 
