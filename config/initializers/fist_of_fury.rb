cebbc5a55811f18083dfc0f8832438582b74887e
James Dong <jdong8@gmail.com>
2014-08-15 10:59:19 -0700

    tweak to fist of fury, emails not sending at all now

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -1,17 +1,16 @@
 # Ensure the jobs run only in a web server.
-#if defined?(Rails::Server)
 
-if Rails.env != "development"
+if Rails.env == "production"
+	if defined?(Rails::Server)
+	  FistOfFury.configure do |config|
+	    config.utc = true # false by default; makes all time within Fist of Fury UTC
+	  end
 
-  FistOfFury.configure do |config|
-    config.utc = true # false by default; makes all time within Fist of Fury UTC
-  end
-
-  FistOfFury.attack! do
-    Inprogress.recurs { daily.hour_of_day(23) }
-    Return.recurs { daily.hour_of_day(5) }
-    # Noresponse.recurs { daily.hour_of_day(23) }
-    Outstanding.recurs { daily.hour_of_day(18) }
-  end
+	  FistOfFury.attack! do
+	    Inprogress.recurs { daily.hour_of_day(23) }
+	    Return.recurs { daily.hour_of_day(5) }
+	    # Noresponse.recurs { daily.hour_of_day(23) }
+	    Outstanding.recurs { daily.hour_of_day(18) }
+	  end
+	end
 end
-#end
