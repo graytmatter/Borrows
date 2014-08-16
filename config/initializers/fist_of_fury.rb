3612ccb56dce8abd020637cf5e76650d07c6b8d6
James Dong <jdong8@gmail.com>
2014-08-15 23:16:51 -0700

    tweak fist of fury

--- config/initializers/fist_of_fury.rb
+++ config/initializers/fist_of_fury.rb
@@ -1,6 +1,5 @@
 # Ensure the jobs run only in a web server.
 
-if Rails.env == "production"
 	if defined?(Rails::Server)
 	  FistOfFury.configure do |config|
 	    config.utc = true # false by default; makes all time within Fist of Fury UTC
@@ -13,4 +12,3 @@ if Rails.env == "production"
 	    Outstanding.recurs { daily.hour_of_day(18) }
 	  end
 	end
-end
