735ba49187ad34342030e8e697ce8618623de1c8
James Dong <jdong8@gmail.com>
2014-08-16 12:05:14 -0700

    set host to PB in actionmailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ GoogleTest::Application.configure do
     password: ENV["GMAIL_PASSWORD"]
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'projectborrow.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
