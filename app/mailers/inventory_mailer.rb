7778804f2050a241efa676a094c3db99d3dc7871
James Dong <jdong8@gmail.com>
2014-08-16 18:03:34 -0700

    restricted mailer to contain only those inventories that have active borrows

--- app/mailers/inventory_mailer.rb
+++ app/mailers/inventory_mailer.rb
@@ -3,7 +3,7 @@ class InventoryMailer < ActionMailer::Base
   def outstanding_request(lender_id)
     @mailer = true
     @lender_email = Signup.find_by_id(lender_id).email
-    @inventories = Signup.find_by_id(lender_id).inventories.where(available: true)
+    @inventories = Signup.find_by_id(lender_id).inventories.where(available: true).select { |i| Borrow.where(status1: 1).where(inventory_id: i).present? }
     # @items_outstanding = Array.new
     # Inventory.where(signup_id: lender_id).select { |i| Borrow.where(inventory_id: i.id).select { |b| b.status1 == 1}.count > 0 }.each { |i| @items_outstanding << Itemlist.find_by_id(i.itemlist_id).name }
     # mail(to: @lender_email, from: ENV['owner'], :subject => "[Project Borrow]: Can someone borrow your stuff?")
