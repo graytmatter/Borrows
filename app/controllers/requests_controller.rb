1514d74c5c82577ff8e68590a2246ba885c1ed11
James Dong <jdong8@gmail.com>
2014-08-23 01:02:49 -0700

    list of inventories showing up on lender infowindow

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -307,7 +307,7 @@ class RequestsController < ApplicationController
     
     @lenders = Signup.where("streetone is not null").select { |s| s.inventories.count > 0 && Geography.find_by_zipcode(s.zipcode).county == Geography.find_by_zipcode(@borrower.zipcode).county && s.id != @borrower.id}
     # gon.watch.lenders = @lenders
-    gon.jbuilder
+    gon.watch.jbuilder
     # at next level of borrows, filter for borrows that are available in the select date ranges)
   end
 
--- app/views/requests/test.json.jbuilder
+++ app/views/requests/test.json.jbuilder
@@ -1,6 +1,8 @@
 json.array!(@lenders) do |json, lender|
   json.(lender, :id, :email, :latitude, :longitude)
   json.inventories lender.inventories do |json, inventory|
-    json.(inventory, :id, :itemlist_id, :description)
+    json.id inventory.id
+    json.name Itemlist.find_by_id(inventory.itemlist_id).name
+    json.description inventory.description 
   end
 end
