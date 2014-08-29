68b3e9144993957b4c109d64aa936dc81b401168
James Dong <jdong8@gmail.com>
2014-08-28 22:04:34 -0700

    updated outstanding mailers to be every 2 days, since given UTC time and randomness of when it happens, every 1 day can actually mean just a few hours a part

--- app/jobs/outstanding.rb
+++ app/jobs/outstanding.rb
@@ -7,7 +7,7 @@ class Outstanding
 
 			# Auto email lenders every other day if they have outstanding requests
 			lender_array = Array.new
-			Inventory.where(id: (Borrow.where(status1:1).all.pluck("inventory_id"))).select { |i| (Date.today - i.signup.last_emailed_on) >= 1}.each { |i| lender_array << i.signup.id }
+			Inventory.where(id: (Borrow.where(status1:1).all.pluck("inventory_id"))).select { |i| (Date.today - i.signup.last_emailed_on) >= 2}.each { |i| lender_array << i.signup.id }
 			lender_array.uniq!
 			lender_array.each { |l|InventoryMailer.outstanding_request(l).deliver }
 
