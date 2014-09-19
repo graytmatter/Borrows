bc2eea5527ff1cb3b990cf18ac1f14ddfe52f17a
James Dong <jdong8@gmail.com>
2014-09-18 23:27:30 -0700

    debugging variables set

--- app/controllers/concerns/generating_oauth.rb
+++ app/controllers/concerns/generating_oauth.rb
@@ -4,6 +4,9 @@ module Generating_oauth
 		if Rails.env == "production"
 			callback_url = "http://www.projectborrow.com/facebook_auth"
 			@oauth = Koala::Facebook::OAuth.new(ENV['Facebook_App_ID'], ENV['Facebook_Secret'], callback_url)
+			puts "CHECK OAUTH"
+			puts ENV['Facebook_App_ID']
+			puts @oauth
 		else
 			callback_url = "http://localhost:3000/facebook_auth"
 			@oauth = Koala::Facebook::OAuth.new(ENV['Facebook_App_ID_test'], ENV['Facebook_Secret_test'], callback_url)
