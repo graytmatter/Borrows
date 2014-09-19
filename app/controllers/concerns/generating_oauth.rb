cd78b3d5eead41497bd7f83c9a66c9dd48153b2b
James Dong <jdong8@gmail.com>
2014-09-18 23:20:03 -0700

    testing why fb login won't work, reverting to test oauth info in production mode

--- app/controllers/concerns/generating_oauth.rb
+++ app/controllers/concerns/generating_oauth.rb
@@ -3,12 +3,9 @@ module Generating_oauth
 	def get_oauth
 		if Rails.env == "production"
 			callback_url = "http://www.projectborrow.com/facebook_auth"
-		else
-			callback_url = "http://localhost:3000/facebook_auth"
-		end
-		if Rails.env == "production"
 			@oauth = Koala::Facebook::OAuth.new(ENV['Facebook_App_ID'], ENV['Facebook_Secret'], callback_url)
 		else
+			callback_url = "http://localhost:3000/facebook_auth"
 			@oauth = Koala::Facebook::OAuth.new(ENV['Facebook_App_ID_test'], ENV['Facebook_Secret_test'], callback_url)
 		end
 	end
