7835f14fbd5447b36116d5d755ffad762f75e60e
James Dong <jdong8@gmail.com>
2014-09-18 23:06:58 -0700

    removed omniauth, set to id/secret to real vs. test fb app

--- app/controllers/concerns/generating_oauth.rb
+++ app/controllers/concerns/generating_oauth.rb
@@ -6,7 +6,11 @@ module Generating_oauth
 		else
 			callback_url = "http://localhost:3000/facebook_auth"
 		end
-		@oauth = Koala::Facebook::OAuth.new(ENV['Facebook_App_ID'], ENV['Facebook_Secret'], callback_url)
+		if Rails.env == "production"
+			@oauth = Koala::Facebook::OAuth.new(ENV['Facebook_App_ID'], ENV['Facebook_Secret'], callback_url)
+		else
+			@oauth = Koala::Facebook::OAuth.new(ENV['Facebook_App_ID_test'], ENV['Facebook_Secret_test'], callback_url)
+		end
 	end
 
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-OmniAuth.config.logger = Rails.logger
+# OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do 
-	  provider :facebook, ENV['Facebook_App_ID'], ENV['Facebook_Secret'],
-	  scope: "user_friends, email, public_profile, user_location", display: "popup"
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do 
+# 	  provider :facebook, ENV['Facebook_App_ID'], ENV['Facebook_Secret'],
+# 	  scope: "user_friends, email, public_profile, user_location", display: "popup"
+# end
