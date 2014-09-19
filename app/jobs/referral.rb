a881cbf52227e7a7921502c711e68dc4bd7563b8
James Dong <jdong8@gmail.com>
2014-09-18 23:51:49 -0700

    debug referral job

--- app/jobs/referral.rb
+++ app/jobs/referral.rb
@@ -2,7 +2,9 @@ class Referral
 	include SuckerPunch::Job
 
 	def perform(emails, referer)
-		ActiveRecord::Base.connection_pool.with_connection do 
+		ActiveRecord::Base.connection_pool.with_connection do
+			puts "IN THE JOB"
+			puts emails 
 			emails.each do |e|
 				unless Signup.where(email: e.strip.downcase).present? || Invitee.where(sent: true).where(email: e.strip.downcase).present?
 	        @invitee = Invitee.find_or_create_by(email: e.strip.downcase) do |i|
