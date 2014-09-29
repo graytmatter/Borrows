a14e73155fae6cce9c922b959ac48d9ea3216524
James Dong <jdong8@gmail.com>
2014-09-29 10:06:20 -0700

    tweak to Cleansignup job

--- app/jobs/cleansignup.rb
+++ app/jobs/cleansignup.rb
@@ -2,7 +2,7 @@ class Cleansignup
 	include SuckerPunch::Job
 	include FistOfFury::Recurrent
 
-	def perform(accepted)
+	def perform
 		ActiveRecord::Base.connection_pool.with_connection do 
 			Signup.where("email is null").where("name is null").destroy_all
 			puts "signups cleaned!"
